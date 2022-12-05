6582fcea1b7bbeba67a23048a515c8ca2b93d86e
Riccardo Gulin <riccardo.gulin@gmail.com>
2022-12-05 13:21:10 +0100

    Adds all files

---
+++ index.js
@@ -0,0 +1,96 @@
+// COMMENTO SU SINGOLA LINEA
+
+console.log("HELLO WORLD!")
+
+/* COMMENTO BLOCCO DI CODICE
+alert("asodlksmadlksa")
+prompt("blablabla")
+confirm("asdasdasd")
+ */
+
+/* let addendo1 = 2.5345
+let addendo2 = 43534
+
+let risultato = addendo1 + addendo2 */
+
+// console.log(risultato)
+
+/* let nome = "Mario"
+let cognome = "Boooh"
+
+let nomeECognome = nome + " " + cognome
+
+console.log(nomeECognome + " e inoltre il risultato è: " + risultato) */
+
+/* let nome = "Mario"
+let cognome = "Boooh"
+
+let nomeECognome = nome + cognome
+
+console.log("Concatenazione di nome e cognome: " + nomeECognome)
+
+let myNumber = 0
+console.log(myNumber) // 0
+myNumber = 1000000 // <-- QUA SOVRASCRIVO IL VALORE PRECEDENTE DI myNumber
+console.log("Valore di myNumber: " + myNumber) // 1000000
+
+let myNumber2 = myNumber
+console.log("Valore di myNumber2: " + myNumber2)
+
+myNumber2 = myNumber2 + 1234564
+
+console.log("Valore di myNumber2: " + myNumber2)
+
+let ten = 10
+
+console.log(ten * ten)
+
+let isAdmin = false */
+
+// isAdmin = 1231
+
+/* console.log("E' un admin? " + isAdmin)
+
+const numero = 13
+const cinque = "5"
+
+console.log(numero + cinque) */
+
+// se qualcosaltro è definito --> lo uso
+// se qualcosaltro è non definito (undefined) --> non lo uso
+
+/* const qualcosa = qualcosaltro */
+
+// ******************************* OPERATORI LOGICI *******************************************
+
+// OPERATORI DI COMPARAZIONE == === (meglio usare la tripla uguaglianza invece della doppia)
+
+const numero = 13
+
+// numero é uguale a 13??
+/* console.log(numero === 14) */
+
+// MAGGIORE (O MAGGIORE UGUALE) & MINORE (O MINORE UGUALE)
+
+/* console.log(numero > 14) // --> false
+console.log(numero >= 14) // > =
+console.log(numero < 14) // --> true
+console.log(numero <= 14) // < = */
+
+// numero è diverso da 0?
+/* console.log(numero !== 0) // ! = = */
+
+// ************************* TYPEOF ********************************
+
+/* console.log(typeof numero) */
+
+// ****************************** OPERAZIONI SU STRINGHE ******************************
+/* 
+const nome = "riccardo"
+
+console.log(nome.toUpperCase())
+
+const cognome = "GULIN"
+
+console.log(cognome.toLowerCase())
+ */
